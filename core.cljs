a139bdb8306270990e3bfe0f70ee2209ac4c0f66
Lyn Headley <laheadle@gmail.com>
2023-03-28 08:08:53 -0700

    c

---
+++ core.cljs
@@ -0,0 +1,30 @@
+(ns static-web.core
+  (:require [reagent.core :as r]
+            [reagent.dom :as dom]
+            [clojure.string :as string]
+            ;; [cljs.pprint :refer [pprint]]
+            ))
+
+(defn line-one []
+  [:h1 [:span.english "You are invited!"] [:span.sep "/"] [:span.spanish "Estás invitado!"]])
+(defn line-two []
+  [:h1 [:span.english "To Hal's Birthday Party!"] [:span.sep "/"] [:span.spanish "¡A la fiesta de cumpleaños de Hal!"]])
+(defn line-three []
+  [:h1 [:span.english "See you there!"] [:span.sep "/"] [:span.spanish "¡Alli nos vemos!"]])
+(defn description []
+  [:h1 [:span.english "Bowling, Games!"] [:span.sep "/"] [:span.spanish "¡Bolos, Juegos!"]])
+
+(defn home []
+  [:div.invitation
+   [line-one]
+   [line-two]
+   [description]
+   [:div.place
+    [:div "April 16th, 2023, 11am-2pm" [:span.sep "/"] "El 16 de Abril, 2023, 11am-2pm"]
+    [:div "Penny Lanes"]
+    [:div "1140 Parkside Main St"]
+    [:div "Cary, NC 27519"]]
+   [line-three]])
+
+
+(dom/render [home] (.getElementById js/document "content"))
